92781bb6eee7987bd1bec8799c5ce84c355434ce
GratG <Grantgroenewaldza@gmail.com>
2022-01-17 18:00:04 +0200

    completed the java script for the game to function

--- javascript.js
+++ javascript.js
@@ -29,31 +29,63 @@ function playRound(playerSelection, computerSelection){
         if(computerSelection === "rock")    
             return("Game is a draw!");
         else if(computerSelection === "scissors")
-            return("You win! Rock beats scissors");
+            return("You win!");// Rock beats scissors
         else
-            return("You lose! Paper beats rock!");
+            return("You lose!");// Paper beats rock
     }
     if(playerSelection === "scissors"){
         if(computerSelection === "scissors")    
             return("Game is a draw!");
         else if(computerSelection === "paper")
-            return("You win! Scissors beats paper!");
+            return("You win!");// Scissors beats paper
         else
-            return("You lose! Rock beats scissors!");
+            return("You lose!");// Rock beats scissors
     }
     if(playerSelection === "paper"){
         if(computerSelection === "paper")    
             return("Game is a draw!");
         else if(computerSelection === "rock")
-            return("You win! Paper beats rock!");
+            return("You win!");// Paper beats rock
         else
-            return("You lose! Scissors beats paper!");
+            return("You lose!");// Scissors beats paper
     }
     
         
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+function game(){
+    console.log("Game will begin now: ");
+    let pScore = 0; //declaring player scores
+    let cScore = 0; //delaring computer score
 
-console.log(playRound(playerSelection, computerSelection));
+    for(let i = 0; i != 5; i++){ //game iterates a total of 5 times
+        const computerSelection = computerPlay();
+        
+        let playerSelection = prompt("choose rock, paper scissors!");
+        if (playRound(playerSelection,computerSelection) === "Game is a draw!")
+            console.log("Match is a draw! no winner!");
+        else if (playRound(playerSelection,computerSelection) === "You win!"){
+            pScore++ ;
+            console.log("You win the match! You now have " + String(pScore) + " points.");
+        }
+        else if (playRound(playerSelection,computerSelection) === "You lose!"){
+            cScore++;
+            console.log("You lost the match! The computer now has " + String(cScore) + " points.");
+        }
+        console.log("Player score: " + String(pScore) + ". Computer score: " + String(cScore))
+    }
+
+    if(pScore > cScore) // returns whether you won, lost or the game was a draw.
+        return(console.log("Congratulations you are the winner!"));
+    else if (pScore < cScore)
+        return(console.log("The winner of this game is the computer! Better luck next time!"));
+    else
+        return(console.log("This game ended in a draw! Well played!"));
+
+}
+//const playerSelection = "rock";
+//const computerSelection = computerPlay();
+
+//console.log(playRound(playerSelection, computerSelection));
+
+game();
